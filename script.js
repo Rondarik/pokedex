de7627cfee3b54e3d6ae14989a26a2a3199b350f
rondarik <rondarik@gmx.de>
2024-03-25 11:10:08 +0100

    add Overview

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let allLoadedPokemons = [];
+let loadedPokemons = [0,1,2,3,4,5,6,7,8,9,10,11];
 let currendPokemon = [];
 let allPokemonNames = [];
 const typeColor =['#A4ACAF', '#D56723', '#3DC7EF', '#B87FC8', '#AB9842', '#808080', '#729F3F', '#7B62A3', '#9EB7B8', '#FD7D24', '#4592C4', '#9BCC50', '#EED535', '#F366B9', '#51C4E7', '#F16E57', '#707070', '#FDB9E9', '#82ABAF', '#3D71A4', ];
@@ -11,6 +12,34 @@ async function init() {
     // await loadAllNames();
     getMainAttributes();
 }
+
+function renderPokemonOverview() {
+    let content = document.getElementById('mainContentID');
+
+    for (let i = 0; i < loadedPokemons.length; i++) {
+        const pokemon = loadedPokemons[i];
+        content.innerHTML += renderOverviewHTML();
+    }
+}
+
+function renderOverviewHTML() {
+    return /*html*/ `
+         <div class="poke_box">
+                <div class="poke_box_header">
+                    <div id="pokeBoxName">name</div>
+                    <div id="pokeBoxId">#id</div>
+                </div>
+                <div class="poke_box_content">
+                    <div class="">
+                        <div>fire</div>
+                        <div>water</div>
+                    </div>
+                    <img id="" src="./img/pokeball.png">
+                </div>
+            </div>
+    `;
+}
+
 
 async function loadPokemonByName() {
     let url = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
